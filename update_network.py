82b820e279af191f9f4b6ff897f65cb86d6919ca
badtux66 <edyp@live.com>
2023-04-15 12:09:16 +0300

    correction1 update_network.py commit

--- update_network.py
+++ update_network.py
@@ -1,52 +1,39 @@
-import re
+import os
 import subprocess
 
-def update_vagrantfile_and_inventory(interface, ip, subnet):
-    with open("Vagrantfile", "r") as f:
-        vagrantfile_lines = f.readlines()
+# Run "ip addr" command and get the network interface name and IP address
+ip_output = subprocess.check_output(["ip", "addr"]).decode("utf-8")
+network_interface = ""
+ip_address = ""
+for line in ip_output.split("\n"):
+    if "inet " in line:
+        ip_address = line.strip().split()[1].split("/")[0]
+    elif "state UP" in line:
+        network_interface = line.strip().split(":")[1].strip()
 
-    with open("Vagrantfile", "w") as f:
-        for line in vagrantfile_lines:
-            if "$network_interface =" in line:
-                line = f"$network_interface = \"{interface}\"\n"
-            f.write(line)
+# Determine the network based on the IP address
+network = ""
+first_octet = int(ip_address.split(".")[0])
+if 192 <= first_octet <= 223:
+    network = f"{first_octet}.0.0.0/8"
+elif 128 <= first_octet <= 191:
+    network = f"{first_octet}.0.0/16"
+else:
+    network = f"{first_octet}.0/24"
 
-    with open("ansible_server/inventory.txt", "r") as f:
-        inventory_lines = f.readlines()
+# Update Vagrantfile
+with open("Vagrantfile", "r") as f:
+    file_contents = f.read()
+file_contents = file_contents.replace('$network_interface = "1: lo"', f'$network_interface = "{network_interface}"')
+file_contents = file_contents.replace('bridge: $network_interface', f'bridge: "{network}"')
+with open("Vagrantfile", "w") as f:
+    f.write(file_contents)
 
-    with open("ansible_server/inventory.txt", "w") as f:
-        for idx, line in enumerate(inventory_lines):
-            if "ansible_ssh_host=" in line:
-                line = re.sub(
-                    r"ansible_ssh_host=\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",
-                    f"ansible_ssh_host={ip}",
-                    line,
-                )
-                ip_parts = ip.split(".")
-                ip_parts[-1] = str(int(ip_parts[-1]) + 1)
-                ip = ".".join(ip_parts)
-            inventory_lines[idx] = line
-        f.writelines(inventory_lines)
-
-try:
-    ip_output = subprocess.check_output("ip addr", shell=True).decode('utf-8')
-    iface_ip_match = re.search(r'(?P<iface>\w+):.*\n.*?inet\s*(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', ip_output)
+# Update inventory file
+with open("inventory.txt", "r") as f:
+    file_contents = f.read()
+file_contents = file_contents.replace("ansible_ssh_host=192.168.174.2", f"ansible_ssh_host={ip_address}")
+with open("inventory.txt", "w") as f:
+    f.write(file_contents)
     
-    if iface_ip_match:
-        iface = iface_ip_match.group("iface")
-        ip = iface_ip_match.group("ip")
-        if ip.startswith("172.17"):
-            raise Exception("Docker IP address found. Ignoring.")
-        subnet = "255.255.255.0"
-        if ip.startswith("10."):
-            subnet = "255.0.0.0"
-        elif ip.startswith("172."):
-            subnet = "255.240.0.0"
-        elif ip.startswith("192.168."):
-            subnet = "255.255.0.0"
-        update_vagrantfile_and_inventory(iface, ip, subnet)
-        print("Vagrantfile and inventory.txt updated.")
-    else:
-        raise Exception("Could not determine network interface or IP address.")
-except Exception as e:
-    print(f"Error: {e}")
+print("Successfully updated Vagrantfile and inventory.txt")
