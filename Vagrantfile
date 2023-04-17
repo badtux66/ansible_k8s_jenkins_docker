4f125760c94c5e9da530e37652e88f392e3a7941
badtux66 <edyp@live.com>
2023-04-17 15:55:32 +0300

    ip route and subdirectory commit

--- Vagrantfile
+++ Vagrantfile
@@ -10,7 +10,7 @@ Vagrant.configure("2") do |config|
   config.vm.define "ansible" do |ansible|
     ansible.vm.box = "generic/ubuntu2004"
     ansible.vm.hostname = "config-server"
-    ansible.vm.network "public_network", bridge: $network_interface, ip: "192.168.174.2", netmask: "255.255.255.0", use_dhcp_assigned_default_route: true
+    ansible.vm.network "public_network", bridge: $network_interface, ip: "192.168.137.4", netmask: "255.255.255.0", use_dhcp_assigned_default_route: true
     ansible.vm.synced_folder "/home/badtux/Desktop/ansible_k8s_jenkins_docker", "/vagrant", type: "rsync", rsync__exclude: ".git/"
 
     ansible.vm.provision "shell", inline: <<-SHELL
@@ -31,7 +31,7 @@ Vagrant.configure("2") do |config|
   config.vm.define "jenkins" do |jenkins|
     jenkins.vm.box = "generic/ubuntu2004"
     jenkins.vm.hostname = "jenkins"
-    jenkins.vm.network "public_network", bridge: $network_interface, ip: "192.168.174.3", netmask: "255.255.255.0", use_dhcp_assigned_default_route: true
+    jenkins.vm.network "public_network", bridge: $network_interface, ip: "192.168.137.4", netmask: "255.255.255.0", use_dhcp_assigned_default_route: true
     jenkins.vm.synced_folder "/home/badtux/Desktop/ansible_k8s_jenkins_docker", "/vagrant", type: "rsync", rsync__exclude: ".git/"
 
     jenkins.vm.provision "shell", inline: <<-SHELL
@@ -49,7 +49,7 @@ Vagrant.configure("2") do |config|
   config.vm.define "k8s" do |k8s|
     k8s.vm.box = "generic/ubuntu2004"
     k8s.vm.hostname = "k8s"
-    k8s.vm.network "public_network", bridge: $network_interface, ip: "192.168.174.5", netmask: "255.255.255.0", use_dhcp_assigned_default_route: true
+    k8s.vm.network "public_network", bridge: $network_interface, ip: "192.168.137.4", netmask: "255.255.255.0", use_dhcp_assigned_default_route: true
     k8s.vm.synced_folder "/home/badtux/Desktop/ansible_k8s_jenkins_docker", "/vagrant", type: "rsync", rsync__exclude: ".git/"
 
     k8s.vm.provision "shell", inline: <<-SHELL
--- update_network.py
+++ update_network.py
@@ -1,39 +1,56 @@
 import os
 import subprocess
 
-# Run "ip addr" command and get the network interface name and IP address
-ip_output = subprocess.check_output(["ip", "addr"]).decode("utf-8")
-network_interface = ""
+# Function to get default network interface
+def get_default_interface():
+    route_output = subprocess.check_output(["ip", "route"]).decode("utf-8")
+    for line in route_output.split("\n"):
+        if "default" in line:
+            return line.split()[4]
+    return None
+
+# Get default network interface and IP address
+default_interface = get_default_interface()
+if default_interface is None:
+    print("Unable to find the default network interface.")
+    exit(1)
+
+ip_output = subprocess.check_output(["ip", "addr", "show", default_interface]).decode("utf-8")
 ip_address = ""
 for line in ip_output.split("\n"):
     if "inet " in line:
         ip_address = line.strip().split()[1].split("/")[0]
-    elif "state UP" in line:
-        network_interface = line.strip().split(":")[1].strip()
+        break
+
+if not ip_address:
+    print("Unable to find IP address associated with the default network interface.")
+    exit(1)
 
 # Determine the network based on the IP address
-network = ""
-first_octet = int(ip_address.split(".")[0])
-if 192 <= first_octet <= 223:
-    network = f"{first_octet}.0.0.0/8"
-elif 128 <= first_octet <= 191:
-    network = f"{first_octet}.0.0/16"
-else:
-    network = f"{first_octet}.0/24"
+ip_parts = ip_address.split(".")
+base_ip = f"{ip_parts[0]}.{ip_parts[1]}.{ip_parts[2]}."
 
 # Update Vagrantfile
 with open("Vagrantfile", "r") as f:
     file_contents = f.read()
-file_contents = file_contents.replace('$network_interface = "1: lo"', f'$network_interface = "{network_interface}"')
-file_contents = file_contents.replace('bridge: "$network_interface"', f'bridge: "{network}"')
+
+file_contents = file_contents.replace('ip: "192.168.174.2"', f'ip: "{base_ip}2"')
+file_contents = file_contents.replace('ip: "192.168.174.3"', f'ip: "{base_ip}3"')
+file_contents = file_contents.replace('ip: "192.168.174.5"', f'ip: "{base_ip}5"')
+
 with open("Vagrantfile", "w") as f:
     f.write(file_contents)
 
 # Update inventory file
-with open("inventory.txt", "r") as f:
+inventory_file_path = "ansible_server/inventory.txt"
+with open(inventory_file_path, "r") as f:
     file_contents = f.read()
-file_contents = file_contents.replace("ansible_ssh_host=192.168.174.2", f"ansible_ssh_host={ip_address}")
-with open("inventory.txt", "w") as f:
-    f.write(file_contents)
-    
+
+updated_file_contents = file_contents.replace("ansible_ssh_host=192.168.174.2", f"ansible_ssh_host={base_ip}2")
+updated_file_contents = updated_file_contents.replace("ansible_ssh_host=192.168.174.3", f"ansible_ssh_host={base_ip}3")
+updated_file_contents = updated_file_contents.replace("ansible_ssh_host=192.168.174.5", f"ansible_ssh_host={base_ip}5")
+
+with open(inventory_file_path, "w") as f:
+    f.write(updated_file_contents)
+
 print("Successfully updated Vagrantfile and inventory.txt")
